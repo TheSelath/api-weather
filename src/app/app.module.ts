94196d4a1a7b37fabf87dbde47ba17e8baecad09
Thales Domingos <thales.domingos93@gmail.com>
2021-01-20 05:26:06 -0300

    implementação da parte 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './pages/home/home.module';
+import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    BookmarksModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
